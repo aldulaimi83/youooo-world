385078977907e0dc781c30e61fc4cdd5783be26c
aldulaimi83 <106041458+aldulaimi83@users.noreply.github.com>
2026-03-16 13:54:25 -0500

    Initial Youooo World map

---
+++ youooo-world/script.js
@@ -0,0 +1,13 @@
+const map = new maplibregl.Map({
+  container: 'map',
+  style: 'https://demotiles.maplibre.org/style.json',
+  center: [14.8,33],
+  zoom:2
+});
+
+map.addControl(new maplibregl.NavigationControl());
+
+new maplibregl.Marker()
+.setLngLat([14.8,33])
+.setPopup(new maplibregl.Popup().setHTML("<h3>Youooo Event</h3><p>Example marker</p>"))
+.addTo(map);
