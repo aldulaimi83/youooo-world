fbda95501f74532cc43af74641186232239d2aea
aldulaimi83 <106041458+aldulaimi83@users.noreply.github.com>
2026-03-16 14:31:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const map = new maplibregl.Map({
   container: 'map',
   style: 'https://demotiles.maplibre.org/style.json',
-  center: [14.8,33],
-  zoom:2
+  center: [14.8, 33],
+  zoom: 2
 });
 
-map.addControl(new maplibregl.NavigationControl());
+map.addControl(new maplibregl.NavigationControl(), 'top-right');
 
 new maplibregl.Marker()
-.setLngLat([14.8,33])
-.setPopup(new maplibregl.Popup().setHTML("<h3>Youooo Event</h3><p>Example marker</p>"))
-.addTo(map);
+  .setLngLat([14.8, 33])
+  .setPopup(
+    new maplibregl.Popup().setHTML(
+      "<h3>Youooo Event</h3><p>Example marker</p>"
+    )
+  )
+  .addTo(map);
